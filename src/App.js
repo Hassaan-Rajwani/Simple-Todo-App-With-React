5c20ebbc4f300602e01dab4fc9ef7f4b21550ae0
Hassaan Rajwani <hassaanqadri258@gmail.com>
2022-01-13 17:16:24 +0500

    Simple Todo App With React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
 import './App.css';
-
+import {useState} from 'react'
 function App() {
+  const[todoItem,setTodoItem] = useState('')
+  const[todo,setTodo] = useState([])
+  const[BtnChange,setBtnChange] = useState(true)
+
+const addTask = () => {
+  if(todoItem == ""){
+    alert("Please Write !")
+  }
+  else{
+    let tem = [...todo]
+    tem.push(todoItem)
+    setTodo(tem)
+    console.log(todo)
+    setTodoItem("")
+  }
+  setBtnChange(true)
+}
+  
+const del = (listIndex) => {
+  console.log(listIndex)
+  const filter = todo.filter((element , index) => {
+    return listIndex !== index
+  })
+  setTodo(filter)
+}
+
+const edit = (listIndex) => {
+  console.log(listIndex)
+  const filter2 = todo.filter((element , index) => {
+    return listIndex == index
+  })
+  setTodoItem(filter2)
+  const filter3 = todo.filter((element , index) => {
+    return listIndex !== index
+  })
+  setTodo(filter3)
+  setBtnChange(false)
+}
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React Todo-App</h1>
+      <br />
+      <input className='input' type="text" onChange={(e)=>{setTodoItem(e.target.value)}} value={todoItem} placeholder="Add Any Item" />
+      {BtnChange ?
+      <button className='btn1' onClick={addTask}>Add</button>
+      :
+      <button className='btn2' onClick={addTask}>Update</button>
+      }
+      <br />
+      <br />
+      <table>
+      {todo.map((item , listIndex) => {
+        return(
+        <tr>
+          <td className='name'>{item}</td>
+          <td><button className='btn3' onClick={()=>{del(listIndex)}}>Delete</button></td>
+          <td><button className='btn4' onClick={() => {edit(listIndex)}}>Edit</button></td>
+        </tr>
+        )
+      })}
+      </table>
     </div>
   );
 }
 
 export default App;
+
