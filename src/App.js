e7411072902c5f6d83912b977369a942381c3345
Hassaan Rajwani <hassaanqadri258@gmail.com>
2022-01-15 19:36:10 +0500

    Modified App.Js

--- src/App.js
+++ src/App.js
@@ -1,53 +1,54 @@
 import './App.css';
 import {useState} from 'react'
+
 function App() {
-  const[todoItem,setTodoItem] = useState('')
+  const[input,setInput] = useState('')
   const[todo,setTodo] = useState([])
   const[BtnChange,setBtnChange] = useState(true)
+  const[editIndex,setEditIndex] = useState()
 
 const addTask = () => {
-  if(todoItem == ""){
+  if(input == ""){
     alert("Please Write !")
   }
   else{
     let tem = [...todo]
-    tem.push(todoItem)
+    tem.push(input)
     setTodo(tem)
     console.log(todo)
-    setTodoItem("")
+    setInput("")
   }
   setBtnChange(true)
 }
   
 const del = (listIndex) => {
-  console.log(listIndex)
-  const filter = todo.filter((element , index) => {
-    return listIndex !== index
-  })
-  setTodo(filter)
+  const tem2 = [...todo]
+    tem2.splice(listIndex , 1)
+    setTodo(tem2)
 }
 
 const edit = (listIndex) => {
-  console.log(listIndex)
-  const filter2 = todo.filter((element , index) => {
-    return listIndex == index
-  })
-  setTodoItem(filter2)
-  const filter3 = todo.filter((element , index) => {
-    return listIndex !== index
-  })
-  setTodo(filter3)
+  setInput(todo[listIndex])
   setBtnChange(false)
+  setEditIndex(listIndex)
 }
+
+const update = () => {
+  const tem3 = [...todo]
+  tem3[editIndex] = input
+  setTodo(tem3)
+  setInput("")
+}
+
   return (
     <div className="App">
       <h1>React Todo-App</h1>
       <br />
-      <input className='input' type="text" onChange={(e)=>{setTodoItem(e.target.value)}} value={todoItem} placeholder="Add Any Item" />
+      <input className='input' type="text" onChange={(e)=>{setInput(e.target.value)}} value={input} placeholder="Add Any Item" />
       {BtnChange ?
       <button className='btn1' onClick={addTask}>Add</button>
       :
-      <button className='btn2' onClick={addTask}>Update</button>
+      <button className='btn2' onClick={update}>Update</button>
       }
       <br />
       <br />
